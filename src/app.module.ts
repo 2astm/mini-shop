f391260aa4a815ea1e117be7b6839f43c4b6051b
Yevhen Molchaniuk <jekajakorev@gmail.com>
2023-01-21 17:50:18 +0200

    Added products module, optimized rootAsync for MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from './config';
+import { ProductsModule } from './product/products.module';
 
 @Module({
   imports: [
@@ -9,13 +10,11 @@ import config from './config';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        console.log(configService.get<string>('MONGODB_URL'));
-        return {
-          uri: configService.get<string>('MONGODB_URL'),
-        };
-      },
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
     }),
+    ProductsModule,
   ],
 })
 export class AppModule {}
